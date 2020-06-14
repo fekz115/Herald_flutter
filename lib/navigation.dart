3e0d9cafec3e394526928bd18cfc081adbd20ff1
fekz115 <fekz115@gmail.com>
2020-06-14 14:57:32 +0300

    flutter: navigation: initial navigation

---
+++ lib/navigation.dart
@@ -0,0 +1,16 @@
+import 'package:Herald_flutter/pages/HomePage.dart';
+import 'package:auto_route/auto_route_annotations.dart';
+import 'package:flutter/material.dart';
+
+@CustomAutoRouter(
+    transitionsBuilder: zoomInTransition,
+    durationInMilliseconds: 400
+  )
+class $Router {
+  @initial
+  HomePage homepage;
+}
+
+Widget zoomInTransition(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
+    return ZoomPageTransitionsBuilder().buildTransitions(null, context, animation, secondaryAnimation, child);
+}
---
+++ lib/navigation.gr.dart
@@ -0,0 +1,42 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouteGenerator
+// **************************************************************************
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:Herald_flutter/pages/HomePage.dart';
+import 'package:Herald_flutter/navigation.dart';
+
+abstract class Routes {
+  static const homepage = '/';
+  static const all = {
+    homepage,
+  };
+}
+
+class Router extends RouterBase {
+  @override
+  Set<String> get allRoutes => Routes.all;
+
+  @Deprecated('call ExtendedNavigator.ofRouter<Router>() directly')
+  static ExtendedNavigatorState get navigator =>
+      ExtendedNavigator.ofRouter<Router>();
+
+  @override
+  Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.homepage:
+        return PageRouteBuilder<dynamic>(
+          pageBuilder: (context, animation, secondaryAnimation) => HomePage(),
+          settings: settings,
+          transitionsBuilder: zoomInTransition,
+          transitionDuration: const Duration(milliseconds: 400),
+        );
+      default:
+        return unknownRoutePage(settings.name);
+    }
+  }
+}
